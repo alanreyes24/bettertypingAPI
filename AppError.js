1a94d79d3bbaa813b6b3996157288806b3fc76b7
Alan Reyes <alanreyes6747@gmail.com>
2024-06-05 11:30:56 -0500

    Add better error display for user

---
+++ AppError.js
@@ -0,0 +1,11 @@
+// AppError.js
+
+class AppError extends Error {
+    constructor(message, statusCode) {
+      super(message);
+      this.statusCode = statusCode;
+    }
+  }
+  
+  module.exports = AppError;
+  
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,24 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const AppError = require('../AppError')
 
-// Error handler
+
+const generateAuthToken = (user) => {
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
+    expiresIn: "1h", 
+  });
+  return token;
+};
+
+
+
 const handleErrors = (err) => {
-  console.log(err.message, err.code);
-  let errors = { username: "", password: "" };
+  console.log("ERROR DETECHED")
+  console.log(err.message);
+  // let errors = { username: "", password: "" };
+  let errors = err.message
+
 
   // Validation errors
   if (err.message.includes("user validation failed")) {
@@ -49,25 +62,32 @@ module.exports.login_get = (req, res) => {
 };
 
 // Login POST
+
 module.exports.login_post = async (req, res) => {
+
   const { username, password } = req.body;
-  console.log("got a request with:");
-  console.log(req.body);
+  console.log("got a request with:")
+  console.log(req.body)
 
   try {
-    const user = await User.findOne({ username: username });
-    if (!user) return res.status(400).json("username or password is incorrect");
+    const user = await User.findOne({ username: username});
+    console.log("this is running")
+    if (user === null) {
+      throw new AppError("Username / Password not found",)
+    }
 
     const validPass = await bcrypt.compare(password, user.password);
     if (!validPass) {
-      console.log("invalid pass");
-      return res.status(400).json("password is incorrect");
+      console.log("invalid pass"); // also not even sure we're supposed to let them know that the password didn't match
+      return res.status(400).json("password is incorrect"); // aren't i suppsoed to throw error instead?
     }
 
-    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    const token = generateAuthToken();
     res.header("auth-token", token).send({ token });
-    console.log("sent token");
+
   } catch (err) {
-    res.status(500).json({ error: "Internal server error" });
+    const errors = handleErrors(err);
+    res.status(400).json(errors);
   }
-};
+
+}
