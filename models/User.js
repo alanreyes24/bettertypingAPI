f33b4e5b690a15cbcae9f241d282ab6594872e40
Alan Reyes <alanreyes6747@gmail.com>
2026-05-24 12:30:49 -0500

    add timestamps to account creation

--- models/User.js
+++ models/User.js
@@ -1,30 +1,22 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const nextAITestSchema = new mongoose.Schema({
-  practiceWords: {
-    type: [String], // Array of strings
-    required: false,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please enter a username"],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter a password"],
+      minlength: [6, "Minimum password length is 6 characters"],
+    },
   },
-});
-
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "Please enter a username"],
-    unique: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please enter a password"],
-    minlength: [6, "Minimum password length is 6 characters"],
-  },
-  nextAITest: {
-    type: nextAITestSchema,
-    required: false,
-  },
-});
+  { timestamps: true }
+);
 
 // Password hashing
 userSchema.pre("save", async function (next) {
